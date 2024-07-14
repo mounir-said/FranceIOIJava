5d11a0f89b5f5648b0f03a8fd1d3aeb8081a1fd5
mounir-said <mou.sai.ms3@gmail.com>
2024-07-14 15:18:31 +0100

    Problem Solving With Java

---
+++ Niveau 1/AffichageDeTexteSuiteDinstructions/Présentation.java
@@ -0,0 +1,7 @@
+class Main {
+   public static void main(String[] args) {
+      System.out.println("Coucou !");
+      System.out.println("Je m'appelle Camthalion");
+      System.out.println("Ma devise est 'Parler peu mais parler bien'.");
+   }
+}
